b12ad50b88851cf409b6c9d3b61fce96d2fdc2ff
D7702_09 <d7702_09@D7702-09ui-iMac.local>
2018-04-03 15:29:51 +0900

    Alien View 03

--- Alien View/ViewController.swift
+++ Alien View/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var counter = 1
+    var chk = true
+    @IBOutlet weak var UIImageView: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //1st 이미지가 출력
+        UIImageView.image = UIImage(named : "frame1.png")
+        lbl.text = "1"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func Updateimage(_ sender: Any) {
+        if counter == 5  {
+            chk = false //내리막길
+        }else if counter == 1   {
+            chk = true  //오르막길
+        }
+        
+        
+        if chk == false {
+            counter = counter - 1
+        }else if chk == true{
+            counter = counter + 1
+        }
+        UIImageView.image = UIImage(named: "frame\(counter).png")
+        lbl.text = String(counter)
     }
-
-
+    
 }
 
